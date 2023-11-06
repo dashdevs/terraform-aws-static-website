b038f039a107c93cd304c7d1f7538314d8138295
Alexander Sorokin <alexander.sorokin@itomy.ch>
2023-11-06 11:16:41 +0200

    RND-124 add outputs for the module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
 output "bucket_id" {
   value = aws_s3_bucket.website.id
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.website.id
+}
+
+output "resource_domain_record" {
+  value = "${var.domain} CNAME ${aws_cloudfront_distribution.website.domain_name}"
+}
+
+output "certificate_validation_records" {
+  value = [
+    for dvo in aws_acm_certificate.website.domain_validation_options : "${dvo.resource_record_name} ${dvo.resource_record_type} ${dvo.resource_record_value}"
+  ]
+}
