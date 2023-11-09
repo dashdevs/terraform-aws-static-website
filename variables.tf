a247785a876c8d4bb4deff3817be03c5e5a0b6f6
Alexander Sorokin <alexander.sorokin@itomy.ch>
2023-11-09 23:26:27 +0200

    Create static website module (#1)

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "domain" {
+  type = string
+}
+
+variable "domain_zone_name" {
+  type    = string
+  default = null
+}
+
+variable "create_dns_records" {
+  type    = bool
+  default = true
+}
+
+variable "cors_allowed_origins" {
+  type    = list(string)
+  default = null
+}
